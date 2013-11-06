90717591f3e3d4d282012325a807eee2399303bf
Jens Schauder <jens.schauder@freenet.de>
2013-11-06 19:12:58 +0100

    Logic class with test

---
+++ src/de/schauderhaft/architecture/example/CrosswordGame.java
@@ -0,0 +1,42 @@
+package de.schauderhaft.architecture.example;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CrosswordGame {
+    private final Set<String> knownWords;
+
+    public CrosswordGame(Set<String> knownWords) {
+        this.knownWords = knownWords;
+    }
+
+    private final Set<String> usedWords = new HashSet<>();
+    private int points;
+
+    public int submit(String word) {
+        if (isWordKnown(word))
+            return points;
+
+        points += calculatePointsForLegalWord(word);
+        return points;
+    }
+
+    private int calculatePointsForLegalWord(String word) {
+        int newPoints = 0;
+        if (wasWordAlreadyUsed(word))
+            newPoints = 1;
+        else {
+            usedWords.add(word);
+            newPoints = word.length();
+        }
+        return newPoints;
+    }
+
+    private boolean wasWordAlreadyUsed(String word) {
+        return usedWords.contains(word);
+    }
+
+    private boolean isWordKnown(String word) {
+        return !knownWords.contains(word);
+    }
+}
---
+++ tst/de/schauderhaft/architecture/example/CrosswordGameTest.java
@@ -0,0 +1,58 @@
+package de.schauderhaft.architecture.example;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class CrosswordGameTest {
+    private final Set<String> knownWords = new HashSet<>();
+    {
+        knownWords.add("word");
+        knownWords.add("crossword");
+    }
+
+    @Test
+    public void newLegalWordResultsInFullPoints() {
+        CrosswordGame game = createGame();
+        int points = game.submit("word");
+        assertEquals(4, points);
+    }
+
+    private CrosswordGame createGame() {
+        return new CrosswordGame(knownWords);
+    }
+
+    @Test
+    public void newLongLegalWordResultsInFullPoints() {
+        CrosswordGame game = createGame();
+        int points = game.submit("crossword");
+        assertEquals(9, points);
+    }
+
+    @Test
+    public void pointsForWordsAddUp() {
+        CrosswordGame game = createGame();
+        game.submit("word");
+        int points = game.submit("crossword");
+        assertEquals(4 + 9, points);
+    }
+
+    @Test
+    public void duplicateWordsGiveOnlySinglePoint() {
+        CrosswordGame game = createGame();
+        int firstPoints = game.submit("word");
+        int secondPoints = game.submit("word");
+        assertEquals(firstPoints + 1, secondPoints);
+    }
+
+    @Test
+    public void zeroPointsForUnknownWords() {
+        CrosswordGame game = createGame();
+        int points = game.submit("dfcguhj");
+        assertEquals(0, points);
+    }
+
+}
