896dcee2f7fd86df4ebf49421c2da21b152b2b45
Thomas Michael <thomas.michael@anmi24.de>
2014-01-18 11:55:33 +0100

    Add Button

--- src/de/schauderhaft/architecture/example/InputView.java
+++ src/de/schauderhaft/architecture/example/InputView.java
@@ -1,5 +1,6 @@
 package de.schauderhaft.architecture.example;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
@@ -8,9 +9,11 @@ public class InputView {
 	JFrame frame = new JFrame("CrossWord");
 	
 	JTextField inputfield = new  JTextField("");
+	JButton submit = new JButton("submit");
 	
 	public  InputView() {
 		frame.getContentPane().add(inputfield);
+		frame.getContentPane().add(submit);
 		frame.pack();
 	}
 	
--- tst/de/schauderhaft/architecture/example/InputViewTest.java
+++ tst/de/schauderhaft/architecture/example/InputViewTest.java
@@ -3,9 +3,11 @@ package de.schauderhaft.architecture.example;
 import java.awt.Component;
 import java.awt.Container;
 
+import javax.swing.JButton;
 import javax.swing.JTextField;
 
 import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class InputViewTest {
@@ -15,15 +17,28 @@ public class InputViewTest {
 		InputView view = new InputView();
 		Component[] components = view.getInputView().getContentPane()
 				.getComponents();
+		componentsAssertTrue(components, JTextField.class);
+
+	}
+
+	@Test
+	public void viewShouldHaveSubmitButton() {
+		InputView view = new InputView();
+		Component[] components = view.getInputView().getContentPane()
+				.getComponents();
+		componentsAssertTrue(components, JButton.class);
+
+	}
+
+	private <T extends Component> void componentsAssertTrue(
+			Component[] components, Class<T> clazz) {
 		boolean fieldExists = false;
+
 		for (Component c : components) {
-			if (c instanceof JTextField) {
+			if (clazz.isInstance(c)){
 				fieldExists = true;
 			}
 		}
 		assertTrue(fieldExists);
-
 	}
-	
-	
 }
