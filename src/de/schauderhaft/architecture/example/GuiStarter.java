e5e6bcb27489db56c812d545bd3f6357fbfd1ee0
Thomas Michael <thomas.michael@anmi24.de>
2014-01-20 07:34:49 +0100

    Adding Exit on Default

--- src/de/schauderhaft/architecture/example/GuiStarter.java
+++ src/de/schauderhaft/architecture/example/GuiStarter.java
@@ -1,10 +1,13 @@
 package de.schauderhaft.architecture.example;
 
+import javax.swing.JFrame;
+
 public class GuiStarter {
 
 	public static void main(String[] args) {
+
 		InputView view = new InputView();
-		
+		view.getInputView().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		view.getInputView().setVisible(true);
 	}
 }
