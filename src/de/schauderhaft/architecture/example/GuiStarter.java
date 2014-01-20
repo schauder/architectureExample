9caa87a738356f3bd9adb7248b510770756bbcd9
Thomas Michael <thomas.michael@anmi24.de>
2014-01-20 06:45:29 +0100

    Adding GUI-Starter, flowlayout and inputfieldsize

---
+++ src/de/schauderhaft/architecture/example/GuiStarter.java
@@ -0,0 +1,10 @@
+package de.schauderhaft.architecture.example;
+
+public class GuiStarter {
+
+	public static void main(String[] args) {
+		InputView view = new InputView();
+		
+		view.getInputView().setVisible(true);
+	}
+}
--- src/de/schauderhaft/architecture/example/InputView.java
+++ src/de/schauderhaft/architecture/example/InputView.java
@@ -1,18 +1,23 @@
 package de.schauderhaft.architecture.example;
 
+import java.awt.FlowLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
 public class InputView {
 
+	private static final String INPUT_LENTGH = "                                         ";
+
 	JFrame frame = new JFrame("CrossWord");
 
-	JTextField inputfield = new JTextField("");
+	JTextField inputfield = new JTextField(INPUT_LENTGH);
 	JButton submit = new JButton("submit");
 	ScoreBoard score = new ScoreBoard();
 
 	public InputView() {
+		frame.getContentPane().setLayout(new FlowLayout());
 		frame.getContentPane().add(inputfield);
 		frame.getContentPane().add(submit);
 		frame.getContentPane().add(score.getScorePanel());
