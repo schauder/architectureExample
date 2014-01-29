1044f5ebcd4108c31a0c818a9810b65fc9bb1fd3
stevenschwenke <steven@stevenschwenke.de>
2014-01-29 06:49:19 +0100

    Game-loop added

--- src/de/schauderhaft/architecture/example/ConsoleStuff.java
+++ src/de/schauderhaft/architecture/example/ConsoleStuff.java
@@ -6,24 +6,27 @@ import java.io.InputStreamReader;
 
 public class ConsoleStuff {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        System.out.println("Spiel gestartet!");
-        System.out.print("Runde 1 Eingabe: ");
-        String input = makeInput();
-        System.out.println(input + " bringt XXX Punkte! Gesamt: XXX");
-    }
+		int runde = 1;
 
-    private static String makeInput() {
-        BufferedReader console = new BufferedReader(new InputStreamReader(
-                System.in));
-        String zeile = null;
-        try {
-            zeile = console.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return zeile;
-    }
+		while (true) {
+			System.out.println("Spiel gestartet!");
+			System.out.print("Runde " + runde++ + " Eingabe: ");
+			String input = makeInput();
+			System.out.println(input + " bringt XXX Punkte! Gesamt: XXX");
+		}
+	}
+
+	private static String makeInput() {
+		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+		String zeile = null;
+		try {
+			zeile = console.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return zeile;
+	}
 
 }
