8c25fc71449efe0544fc6337f9b8b44eaae8b7c6
KroneckerDelta <kroneckerdelta@thomasmichael.de>
2014-02-16 21:36:00 +0100

    Hazelcast hinzugefügt, aber noch nicht benutzt

--- src/de/schauderhaft/architecture/example/CrosswordGame.java
+++ src/de/schauderhaft/architecture/example/CrosswordGame.java
@@ -1,42 +1,48 @@
 package de.schauderhaft.architecture.example;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-public class CrosswordGame {
+public class CrosswordGame implements Serializable {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
     private final Set<String> knownWords;
 
     public CrosswordGame(Set<String> knownWords) {
-        this.knownWords = knownWords;
+	this.knownWords = knownWords;
     }
 
     private final Set<String> usedWords = new HashSet<String>();
     private int points;
 
     public int submit(String word) {
-        if (isWordKnown(word))
-            return points;
+	if (isWordKnown(word))
+	    return points;
 
-        points += calculatePointsForLegalWord(word);
-        return points;
+	points += calculatePointsForLegalWord(word);
+	return points;
     }
 
     private int calculatePointsForLegalWord(String word) {
-        int newPoints = 0;
-        if (wasWordAlreadyUsed(word))
-            newPoints = 1;
-        else {
-            usedWords.add(word);
-            newPoints = word.length();
-        }
-        return newPoints;
+	int newPoints = 0;
+	if (wasWordAlreadyUsed(word))
+	    newPoints = 1;
+	else {
+	    usedWords.add(word);
+	    newPoints = word.length();
+	}
+	return newPoints;
     }
 
     private boolean wasWordAlreadyUsed(String word) {
-        return usedWords.contains(word);
+	return usedWords.contains(word);
     }
 
     private boolean isWordKnown(String word) {
-        return !knownWords.contains(word);
+	return !knownWords.contains(word);
     }
 }
