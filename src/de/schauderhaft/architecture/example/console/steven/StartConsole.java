aec317e2dc7f262ae1fe3b41e5a13511822d53fb
Steven Schwenke <steven@stevenschwenke.de>
2014-03-01 11:15:24 +0100

    Housekeeping

--- src/de/schauderhaft/architecture/example/console/steven/StartConsole.java
+++ src/de/schauderhaft/architecture/example/console/steven/StartConsole.java
@@ -1,4 +1,4 @@
-package de.schauderhaft.architecture.example;
+package de.schauderhaft.architecture.example.console.steven;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -8,7 +8,9 @@ import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
 
-public class ConsoleStuff {
+import de.schauderhaft.architecture.example.CrosswordGame;
+
+public class StartConsole {
 
 	private static CrosswordGame game;
 
