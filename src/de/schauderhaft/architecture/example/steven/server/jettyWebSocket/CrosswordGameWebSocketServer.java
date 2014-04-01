1318a5f9b72c45eccb0babc8ca2f917f3e9a965d
Steven Schwenke <steven@stevenschwenke.de>
2014-04-01 17:54:34 +0200

    Each web socket builds up his own dictionary

--- src/de/schauderhaft/architecture/example/steven/server/jettyWebSocket/CrosswordGameWebSocketServer.java
+++ src/de/schauderhaft/architecture/example/steven/server/jettyWebSocket/CrosswordGameWebSocketServer.java
@@ -1,12 +1,5 @@
 package de.schauderhaft.architecture.example.steven.server.jettyWebSocket;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
-
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.websocket.server.WebSocketHandler;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
@@ -15,22 +8,11 @@ public class CrosswordGameWebSocketServer {
 
 	private static final int PORT = 8080;
 
-	HashSet<String> knownWords;
-
 	public static void main(String[] args) throws Exception {
 		new CrosswordGameWebSocketServer();
 	}
 
 	public CrosswordGameWebSocketServer() {
-
-		System.out.println("Filling dictionary");
-		try {
-			knownWords = new HashSet<String>();
-			fillDictionary(knownWords);
-		} catch (IOException e) {
-			System.out.println("Exception filling dictionary: " + e.getMessage());
-			e.printStackTrace();
-		}
 
 		System.out.println("Starting web server at port " + PORT);
 		try {
@@ -41,40 +23,13 @@ public class CrosswordGameWebSocketServer {
 		}
 	}
 
-	private void fillDictionary(HashSet<String> knownWords) throws IOException {
-		knownWords.addAll(readFile("ressource/dictionary/altamer.0"));
-		knownWords.addAll(readFile("ressource/dictionary/altamer.1"));
-		knownWords.addAll(readFile("ressource/dictionary/altamer.2"));
-		knownWords.addAll(readFile("ressource/dictionary/american.0"));
-		knownWords.addAll(readFile("ressource/dictionary/american.1"));
-		knownWords.addAll(readFile("ressource/dictionary/american.2"));
-		knownWords.addAll(readFile("ressource/dictionary/british.0"));
-		knownWords.addAll(readFile("ressource/dictionary/british.1"));
-		knownWords.addAll(readFile("ressource/dictionary/british.2"));
-		knownWords.addAll(readFile("ressource/dictionary/english.0"));
-		knownWords.addAll(readFile("ressource/dictionary/english.1"));
-		knownWords.addAll(readFile("ressource/dictionary/english.2"));
-		knownWords.addAll(readFile("ressource/dictionary/english.3"));
-
-		System.out.println("There are " + knownWords.size() + " words in the dictionary.\n");
-	}
-
-	private Set<String> readFile(String path) throws IOException {
-		HashSet<String> set = new HashSet<String>();
-		BufferedReader br = new BufferedReader(new FileReader(new File(path)));
-		String line;
-		while ((line = br.readLine()) != null) {
-			set.add(line);
-		}
-		br.close();
-		return set;
-	}
-
 	private void startWebServer() throws Exception, InterruptedException {
 		Server server = new Server(PORT);
 		WebSocketHandler wsHandler = new WebSocketHandler() {
 			@Override
 			public void configure(WebSocketServletFactory factory) {
+				// this is the class that shall be instantiated for each
+				// incoming websocket upgrade request
 				factory.register(MyWebSocketHandler.class);
 			}
 		};
--- src/de/schauderhaft/architecture/example/steven/server/jettyWebSocket/MyWebSocketHandler.java
+++ src/de/schauderhaft/architecture/example/steven/server/jettyWebSocket/MyWebSocketHandler.java
@@ -1,6 +1,8 @@
 package de.schauderhaft.architecture.example.steven.server.jettyWebSocket;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
@@ -9,25 +11,32 @@ import org.eclipse.jetty.websocket.api.annotations.OnWebSocketError;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 
+import de.schauderhaft.architecture.example.CrosswordGame;
+
 @WebSocket
 public class MyWebSocketHandler {
 
-	@OnWebSocketClose
-	public void onClose(Session session, int statusCode, String reason) {
-		System.out.println("Closed connection from " + session.getRemoteAddress().getAddress() + ":" + session.getRemoteAddress().getPort()
-				+ " because of code " + statusCode + " (" + reason + ")");
-	}
+	Set<String> knownWords;
+	private Map<String, CrosswordGame> games;
 
-	@OnWebSocketError
-	public void onError(Session session, Throwable t) {
-		System.out.println("Error with client " + session.getRemoteAddress().getAddress() + ": " + t.getMessage());
+	public MyWebSocketHandler() {
+
+		System.out.print("New connection detected! Instantiating new WebSocket, filling dictionary. ");
+		try {
+			knownWords = Helper.fillDictionary();
+			System.out.println("There are " + knownWords.size() + " words in the dictionary.");
+		} catch (IOException e) {
+			System.out.println("Exception filling dictionary: " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 
 	@OnWebSocketConnect
 	public void onConnect(Session session) {
-		System.out.println("New connection from " + session.getRemoteAddress().getAddress() + ":" + session.getRemoteAddress().getPort()
-				+ ", sending welcome message");
+		String clientIdentifier = extractClientIdentifier(session);
+		System.out.println("New connection from " + clientIdentifier + ", sending welcome message");
 		try {
+			// games.put(clientIdentifier, new CrosswordGame(knownWords));
 			session.getRemote().sendString("Welcome, client! I started a new game for you and am ready to receive your words.");
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -38,4 +47,29 @@ public class MyWebSocketHandler {
 	public void onMessage(Session session, String message) {
 		System.out.println("Message from " + session.getRemoteAddress().getAddress() + ":" + session.getRemoteAddress().getPort() + ": " + message);
 	}
+
+	@OnWebSocketClose
+	public void onClose(Session session, int statusCode, String reason) {
+		System.out.println("Closed connection from " + session.getRemoteAddress().getAddress() + ":" + session.getRemoteAddress().getPort()
+				+ " because of code " + statusCode + " (" + reason + ")");
+	}
+
+	@OnWebSocketError
+	public void onError(Session session, Throwable t) {
+		System.out.println("Error with client " + session.getRemoteAddress().getAddress() + ":" + session.getRemoteAddress().getPort() + ": " + t.getMessage());
+	}
+
+	/**
+	 * Extracts an identifier string from a session so that every connection can
+	 * be identified.
+	 * 
+	 * @param session
+	 *            from which to extract the identifier
+	 * @return string identifier
+	 */
+	private String extractClientIdentifier(Session session) {
+		String clientIdentifier = session.getRemoteAddress().getAddress() + ":" + session.getRemoteAddress().getPort();
+		return clientIdentifier;
+	}
+
 }
