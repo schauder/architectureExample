1ee54306e4b62edb36d7f9ac5bfbaa28edc44877
Jens Schauder <jens.schauder@freenet.de>
2014-06-12 13:59:48 +0200

    split Client in client and handler

--- src/main/java/de/schauderhaft/architecture/example/steven/client/jettyWebSocket/Client.java
+++ src/main/java/de/schauderhaft/architecture/example/steven/client/jettyWebSocket/Client.java
@@ -1,80 +1,34 @@
 package de.schauderhaft.architecture.example.steven.client.jettyWebSocket;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-import org.eclipse.jetty.websocket.api.Session;
-import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
-import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
-import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
-import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
 import org.eclipse.jetty.websocket.client.WebSocketClient;
 
 public class Client {
 
-	public static void main(String[] args) throws IOException, URISyntaxException {
-		new Client().run(new URI("ws://localhost:8080/"));
-	}
+    public static void main(String[] args) throws IOException,
+            URISyntaxException {
+        new Client().run(new URI("ws://localhost:8080/"));
+    }
 
-	public void run(URI destinationUri) throws IOException {
+    public void run(URI destinationUri) throws IOException {
 
-		WebSocketClient client = new WebSocketClient();
-		MyWebSocket socket = new MyWebSocket();
-		try {
-			client.start();
-			ClientUpgradeRequest request = new ClientUpgradeRequest();
-			System.out.println("Connecting to " + destinationUri);
-			client.connect(socket, destinationUri, request);
+        WebSocketClient client = new WebSocketClient();
+        MyWebSocketClientHandler socketHandler = new MyWebSocketClientHandler();
+        try {
+            client.start();
+            ClientUpgradeRequest request = new ClientUpgradeRequest();
+            System.out.println("Connecting to " + destinationUri);
+            client.connect(socketHandler, destinationUri, request);
 
-			socket.awaitClose(5, TimeUnit.SECONDS);
-		} catch (Throwable t) {
-			t.printStackTrace();
-		}
-	}
-
-	private static String makeInput() {
-		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-		String zeile = null;
-		try {
-			zeile = console.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return zeile;
-	}
-
-	@WebSocket
-	public class MyWebSocket {
-		private final CountDownLatch closeLatch = new CountDownLatch(1);
-
-		@OnWebSocketConnect
-		public void onConnect(Session session) {
-			System.out.println("Connection established. Ready to send words to server");
-		}
-
-		@OnWebSocketMessage
-		public void onMessage(Session session, String message) throws IOException {
-
-			System.out.println(message);
-
-			String input = makeInput();
-			session.getRemote().sendString(input);
-		}
-
-		@OnWebSocketClose
-		public void onClose(int statusCode, String reason) {
-			System.out.println("WebSocket Closed. Code:" + statusCode);
-		}
-
-		public boolean awaitClose(int duration, TimeUnit unit) throws InterruptedException {
-			return this.closeLatch.await(duration, unit);
-		}
-	}
+            socketHandler.awaitClose(5, TimeUnit.SECONDS);
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
 
 }
---
+++ src/main/java/de/schauderhaft/architecture/example/steven/client/jettyWebSocket/MyWebSocketClientHandler.java
@@ -0,0 +1,57 @@
+package de.schauderhaft.architecture.example.steven.client.jettyWebSocket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+@WebSocket
+public class MyWebSocketClientHandler {
+
+    private final CountDownLatch closeLatch = new CountDownLatch(1);
+
+    @OnWebSocketConnect
+    public void onConnect(Session session) {
+        System.out
+                .println("Connection established. Ready to send words to server");
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+
+        System.out.println(message);
+
+        String input = makeInput();
+        session.getRemote().sendString(input);
+    }
+
+    private static String makeInput() {
+        BufferedReader console = new BufferedReader(new InputStreamReader(
+                System.in));
+        String zeile = null;
+        try {
+            zeile = console.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return zeile;
+    }
+
+    @OnWebSocketClose
+    public void onClose(int statusCode, String reason) {
+        System.out.println("WebSocket Closed. Code:" + statusCode);
+    }
+
+    public boolean awaitClose(int duration, TimeUnit unit)
+            throws InterruptedException {
+        return this.closeLatch.await(duration, unit);
+    }
+
+}
